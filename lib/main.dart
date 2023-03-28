ef28fc1d8724f67792ae2effc4c6d98d456eb599
Alžbeta Hajná <xhajna@fi.muni.cz>
2023-03-28 16:33:03 +0200

    fix: use system themeMode

--- lib/main.dart
+++ lib/main.dart
@@ -46,7 +46,7 @@ class MyApp extends StatelessWidget {
             // To use the Playground font, add GoogleFonts package and uncomment
             // fontFamily: GoogleFonts.notoSans().fontFamily,
           ),
-          themeMode: ThemeMode.light,
+          themeMode: ThemeMode.system,
           home: MainPage(),
         ));
   }
