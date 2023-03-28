8ca9a7dbc3217cc2eb86478c5d3ea014ad68938f
Alžbeta Hajná <xhajna@fi.muni.cz>
2023-03-28 17:09:04 +0200

    feat: custom theme to match default dart colors

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,16 @@ class MyApp extends StatelessWidget {
         child: MaterialApp(
           title: 'Flutter Demo',
           theme: FlexThemeData.light(
-            scheme: FlexScheme.amber,
+            colors: const FlexSchemeColor(
+              primary: Color(0xff8bc34a),
+              primaryContainer: Color(0xffb7dc94),
+              secondary: Color(0xffac3306),
+              secondaryContainer: Color(0xffffdbcf),
+              tertiary: Color(0xfffdd877),
+              tertiaryContainer: Color(0xffb59538),
+              appBarColor: Color(0xffffdbcf),
+              error: Color(0xffb00020),
+            ),
             surfaceMode: FlexSurfaceMode.levelSurfacesLowScaffold,
             blendLevel: 9,
             subThemesData: const FlexSubThemesData(
@@ -31,7 +40,16 @@ class MyApp extends StatelessWidget {
             // fontFamily: GoogleFonts.notoSans().fontFamily,
           ),
           darkTheme: FlexThemeData.dark(
-            scheme: FlexScheme.amber,
+            colors: const FlexSchemeColor(
+              primary: Color(0xff8bc34a),
+              primaryContainer: Color(0xffb7dc94),
+              secondary: Color(0xffee5444),
+              secondaryContainer: Color(0xffc36142),
+              tertiary: Color(0xffd5a42e),
+              tertiaryContainer: Color(0xffffd682),
+              appBarColor: Color(0xffc36142),
+              error: Color(0xffcf6679),
+            ),
             surfaceMode: FlexSurfaceMode.levelSurfacesLowScaffold,
             blendLevel: 15,
             subThemesData: const FlexSubThemesData(
@@ -39,6 +57,7 @@ class MyApp extends StatelessWidget {
               appBarBackgroundSchemeColor: SchemeColor.primary,
             ),
             visualDensity: FlexColorScheme.comfortablePlatformDensity,
+            swapLegacyOnMaterial3: true,
             // To use the Playground font, add GoogleFonts package and uncomment
             // fontFamily: GoogleFonts.notoSans().fontFamily,
           ),
