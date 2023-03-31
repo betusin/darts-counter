2845201b104e914cba1af7ccc2b8465bb7825ab8
Alžbeta Hajná <xhajna@fi.muni.cz>
2023-03-31 21:14:08 +0200

    feat: graph with dummy data

--- lib/widgets/statistics_graph.dart
+++ lib/widgets/statistics_graph.dart
@@ -1,15 +1,41 @@
+import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 
 class StatisticsGraph extends StatelessWidget {
   const StatisticsGraph({Key? key}) : super(key: key);
 
+  LineChartData get sampleData => LineChartData(
+        lineBarsData: lineBarsData1,
+        minX: 0,
+        maxX: 14,
+        maxY: 100,
+        minY: 0,
+      );
+
+  List<LineChartBarData> get lineBarsData1 => [
+        lineChartBarData1_1,
+      ];
+
+  LineChartBarData get lineChartBarData1_1 => LineChartBarData(
+        isCurved: true,
+        barWidth: 4,
+        color: Colors.blue,
+        isStrokeCapRound: true,
+        dotData: FlDotData(show: false),
+        belowBarData: BarAreaData(show: false),
+        spots: const [
+          FlSpot(1, 1),
+          FlSpot(3, 20),
+          FlSpot(5, 40),
+          FlSpot(7, 30),
+          FlSpot(10, 80),
+          FlSpot(12, 55),
+          FlSpot(13, 45),
+        ],
+      );
+
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Text("Average in last 100 games"),
-        Text("GRAPH"),
-      ],
-    );
+    return Expanded(child: LineChart(sampleData));
   }
 }
