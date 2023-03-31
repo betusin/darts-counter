695988568cbecde158fa3271dca184cd5a2022b2
Alžbeta Hajná <xhajna@fi.muni.cz>
2023-03-31 16:12:28 +0200

    feat: main page with redirection

---
+++ lib/pages/main_page.dart
@@ -0,0 +1,31 @@
+import 'package:dartboard/board/dart_board.dart';
+import 'package:flutter/material.dart';
+
+class MainPage extends StatelessWidget {
+  const MainPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Main Page"),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            TextButton(
+                onPressed: () {
+                  final pageToPush =
+                      MaterialPageRoute(builder: (_) => DartBoard());
+                  Navigator.of(context).push(pageToPush);
+                },
+                child: Text("New Local Game")),
+            TextButton(onPressed: () {}, child: Text("New Online Game")),
+            TextButton(onPressed: () {}, child: Text("Settings")),
+            TextButton(onPressed: () {}, child: Text("Exit App")),
+          ],
+        ),
+      ),
+    );
+  }
+}
