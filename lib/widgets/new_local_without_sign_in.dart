0f62070beda5328dd72ac3c9f6b19588da0e6a2d
Alžbeta Hajná <xhajna@fi.muni.cz>
2023-04-19 18:39:50 +0200

    fix: add text to play local game

--- lib/widgets/new_local_without_sign_in.dart
+++ lib/widgets/new_local_without_sign_in.dart
@@ -7,9 +7,15 @@ class NewLocalWithoutSignIn extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(12.0),
-      child: ElevatedButton(
-        onPressed: () => Navigator.pushNamed(context, '/game/local'),
-        child: Text("New Local Game"),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Text("Wanna play immediately?"),
+          ElevatedButton(
+            onPressed: () => Navigator.pushNamed(context, '/game/local'),
+            child: Text("Play Local Game"),
+          ),
+        ],
       ),
     );
   }
