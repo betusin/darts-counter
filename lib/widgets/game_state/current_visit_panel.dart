ae8dbb50a71ed05899bbee00669f0d7206cb807d
Alžbeta Hajná <xhajna@fi.muni.cz>
2023-04-24 14:42:06 +0200

    fix(UI): add padding for score container to have some divider between containers if display too small

--- lib/widgets/game_state/current_visit_panel.dart
+++ lib/widgets/game_state/current_visit_panel.dart
@@ -20,17 +20,20 @@ class CurrentVisitPanel extends StatelessWidget {
   }
 
   Widget _buildScoreContainer(String text) {
-    return Container(
-      alignment: Alignment.center,
-      width: 55,
-      height: 70,
-      color: Colors.black,
-      child: Text(
-        text,
-        style: TextStyle(
-            color: visit.isBusted ? Colors.red : Colors.white,
-            fontSize: 30,
-            fontWeight: FontWeight.bold),
+    return Padding(
+      padding: const EdgeInsets.all(0.5),
+      child: Container(
+        alignment: Alignment.center,
+        width: 55,
+        height: 70,
+        color: Colors.black,
+        child: Text(
+          text,
+          style: TextStyle(
+              color: visit.isBusted ? Colors.red : Colors.white,
+              fontSize: 30,
+              fontWeight: FontWeight.bold),
+        ),
       ),
     );
   }
