a1b412a0d619df483c9af3fbd27f60bb47073b69
Alžbeta Hajná <xhajna@fi.muni.cz>
2023-05-09 18:22:55 +0200

    refactor: moved listen on invites into online game

--- lib/pages/main_page.dart
+++ lib/pages/main_page.dart
@@ -1,8 +1,4 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:dartboard/service/invite_service.dart';
-import 'package:dartboard/service/ioc_container.dart';
 import 'package:dartboard/widgets/grid_redirect_button.dart';
-import 'package:dartboard/widgets/invite_list_item.dart';
 import 'package:flutter/material.dart';
 
 import '../widgets/profile_bar.dart';
@@ -23,7 +19,6 @@ class MainPage extends StatelessWidget {
               name: "John",
               surname: "Doe",
             ),
-            _buildInvites(context),
             _buildButtons(context),
           ],
         ),
@@ -57,46 +52,6 @@ class MainPage extends StatelessWidget {
             iconData: Icons.exit_to_app,
           ),
         ],
-      ),
-    );
-  }
-
-  Expanded _buildInvites(
-    BuildContext context,
-  ) {
-    final inviteController = get<InviteService>();
-
-    return Expanded(
-      child: StreamBuilder<QuerySnapshot>(
-        stream: inviteController.invites,
-        builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
-          if (snapshot.hasError) {
-            return const Text("Error occurred");
-          }
-
-          if (!snapshot.hasData) {
-            return const Center(
-              child: CircularProgressIndicator(),
-            );
-          }
-
-          final docs = snapshot.data!.docs;
-
-          return ListView.separated(
-            itemBuilder: (context, index) {
-              var docData = docs[index].data();
-              return InviteListItem(
-                inviteFrom: docData['inviteFrom'],
-                validUntil: docData['validUntil'],
-                inviteID: docs[index].id,
-              );
-            },
-            separatorBuilder: (BuildContext context, int index) {
-              return Divider();
-            },
-            itemCount: docs.length,
-          );
-        },
       ),
     );
   }
--- lib/pages/online_game_start_page.dart
+++ lib/pages/online_game_start_page.dart
@@ -1,6 +1,9 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:dartboard/service/invite_service.dart';
 import 'package:dartboard/service/ioc_container.dart';
 import 'package:flutter/material.dart';
+
+import '../widgets/invite_list_item.dart';
 
 class OnlineGameStartPage extends StatelessWidget {
   const OnlineGameStartPage({Key? key}) : super(key: key);
@@ -28,11 +31,52 @@ class OnlineGameStartPage extends StatelessWidget {
             },
             child: Text("Invite friend"),
           ),
+          _buildInvites(context),
           ElevatedButton(
             onPressed: () => Navigator.pushNamed(context, "/game/online"),
             child: Text("Start game!"),
           )
         ],
+      ),
+    );
+  }
+
+  Expanded _buildInvites(
+    BuildContext context,
+  ) {
+    final inviteController = get<InviteService>();
+
+    return Expanded(
+      child: StreamBuilder<QuerySnapshot>(
+        stream: inviteController.invites,
+        builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
+          if (snapshot.hasError) {
+            return const Text("Error occurred");
+          }
+
+          if (!snapshot.hasData) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+
+          final docs = snapshot.data!.docs;
+
+          return ListView.separated(
+            itemBuilder: (context, index) {
+              var docData = docs[index].data();
+              return InviteListItem(
+                inviteFrom: docData['inviteFrom'],
+                validUntil: docData['validUntil'],
+                inviteID: docs[index].id,
+              );
+            },
+            separatorBuilder: (BuildContext context, int index) {
+              return Divider();
+            },
+            itemCount: docs.length,
+          );
+        },
       ),
     );
   }
