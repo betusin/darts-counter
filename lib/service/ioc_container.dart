bf1189a6e415812159af6b6f928cb06754719c07
Alžbeta Hajná <xhajna@fi.muni.cz>
2023-05-16 22:44:22 +0200

    feat: delete old unused statistics service

--- lib/service/ioc_container.dart
+++ lib/service/ioc_container.dart
@@ -2,7 +2,6 @@ import 'package:dartboard/service/game_service.dart';
 import 'package:dartboard/service/game_statistics_service.dart';
 import 'package:dartboard/service/invite_service.dart';
 import 'package:dartboard/service/setup_user_service.dart';
-import 'package:dartboard/service/statistic_service.dart';
 import 'package:dartboard/service/toast_service.dart';
 import 'package:get_it/get_it.dart';
 
@@ -12,9 +11,6 @@ class IoCContainer {
   Future<void> setup() async {
     get.registerSingleton(
       ToastService(),
-    );
-    get.registerSingleton(
-      StatisticService(),
     );
     get.registerSingleton(
       SetupUserService(),
