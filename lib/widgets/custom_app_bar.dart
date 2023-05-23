a203a3ea5134986c7ebbc8af9a9fb8def8787384
Alžbeta Hajná <xhajna@fi.muni.cz>
2023-05-23 20:59:18 +0200

    fix: if on page statistics do not display redirect btn to statistics

--- lib/widgets/custom_app_bar.dart
+++ lib/widgets/custom_app_bar.dart
@@ -2,6 +2,8 @@ import 'package:dartboard/widgets/profile/avatar.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
+const String STATISTICS_LOCATION = '/statistics';
+
 class CustomAppBar extends AppBar {
   final Widget title;
   final BuildContext context;
@@ -14,14 +16,16 @@ class CustomAppBar extends AppBar {
           key: key,
           title: title,
           actions: [
-            IconButton(
+            if (GoRouter.of(context).location != STATISTICS_LOCATION)
+              IconButton(
                 onPressed: () {
-                  context.push('/statistics');
+                  context.push(STATISTICS_LOCATION);
                 },
                 icon: const Icon(
                   Icons.align_vertical_bottom_rounded,
                   size: 36,
-                )),
+                ),
+              ),
             Avatar(),
           ],
         );
